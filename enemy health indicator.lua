8ad8bc9a4dddd1b9daba16e36308499a768dd1eb
Zach Amador <zach.amador@gmail.com>
2022-09-02 19:44:01 -0700

    Enemy health updates when hurt

--- enemy health indicator.lua
+++ enemy health indicator.lua
@@ -3,8 +3,9 @@
 
 Triggers = {}
 
-OFFSET = 1.0
-DIGIT_SPACE = 0.05
+OFFSET = 0.5
+DIGIT_SPACE = 0.07
+COLLECTION = 7
 SEQ_IDX = 34
 
 function Triggers.init()
@@ -14,27 +15,37 @@ end
 function Triggers.idle()
   for m in Monsters() do
     if m.visible and not m.player then
-      if m._label == nil and m.life > 0 then
-        m._lifelabel = m.life
-        local life_str = string.format("%d", m.life)
-        
-        -- create label
-        local h = MonsterTypes[m.type.mnemonic].height + OFFSET
-        m._labellen = #life_str
-        m._label = {}
-        for i = 1, #life_str do
-          local seq = SEQ_IDX + tonumber(life_str:sub(i, i))
-          m._label[i] = Ephemera.new(m.x - (i-1)*DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon, 7, seq, m.facing)
+      if m.life > 0 then
+        if m._label == nil then
+          m._label = {}
+          m._lifelabel = 0
+          m._labellen = 0
         end
-        --m._label = {
-        --  Ephemera.new(m.x, m.y, m.z + OFFSET, m.polygon, 7, 35, m.facing),
-        --  Ephemera.new(m.x - DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon, 7, 34, m.facing)
-        --}
-      end
-      if m._label ~= nil then
-        for i = 1, m._labellen do
-          m._label[i]:position(m.x - (i-1)*DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon)
-          m._label[i].facing = m.facing
+        if m._lifelabel ~= m.life then
+          -- label needs updating
+          
+          -- delete old ephemera
+          for i = 1, m._labellen do
+            m._label[i]:delete()
+          end
+          
+          m._lifelabel = m.life
+          
+          local life_str = string.format("%d", m.life)
+          
+          -- create new ephemera
+          m._labellen = #life_str
+          for i = 1, #life_str do
+            local seq = SEQ_IDX + tonumber(life_str:sub(i, i))
+            m._label[i] = Ephemera.new(0, 0, 0, 0, COLLECTION, seq, m.facing)
+          end
+        end
+        if m._label ~= nil then
+          local h = MonsterTypes[m.type.mnemonic].height + OFFSET
+          for i = 1, m._labellen do
+            m._label[i]:position(m.x - (i-1)*DIGIT_SPACE, m.y, m.z + h, m.polygon)
+            m._label[i].facing = m.facing
+          end
         end
       end
     end
