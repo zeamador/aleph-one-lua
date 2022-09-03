7363ddd7b57d87e88121f6d2460e7d6b04e713a0
Zach Amador <zach.amador@gmail.com>
2022-09-02 19:27:35 -0700

    Enemy health indicator actually shows (spawn) health

--- enemy health indicator.lua
+++ enemy health indicator.lua
@@ -4,6 +4,8 @@
 Triggers = {}
 
 OFFSET = 1.0
+DIGIT_SPACE = 0.05
+SEQ_IDX = 34
 
 function Triggers.init()
 	Game.restore_passed()
@@ -12,14 +14,28 @@ end
 function Triggers.idle()
   for m in Monsters() do
     if m.visible and not m.player then
-      if m._label == nil and m.life ~= 0 then
+      if m._label == nil and m.life > 0 then
+        m._lifelabel = m.life
+        local life_str = string.format("%d", m.life)
+        
         -- create label
         local h = MonsterTypes[m.type.mnemonic].height + OFFSET
-        m._label = Ephemera.new(m.x, m.y, m.z + OFFSET, m.polygon, 7, 34, m.facing)
+        m._labellen = #life_str
+        m._label = {}
+        for i = 1, #life_str do
+          local seq = SEQ_IDX + tonumber(life_str:sub(i, i))
+          m._label[i] = Ephemera.new(m.x - (i-1)*DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon, 7, seq, m.facing)
+        end
+        --m._label = {
+        --  Ephemera.new(m.x, m.y, m.z + OFFSET, m.polygon, 7, 35, m.facing),
+        --  Ephemera.new(m.x - DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon, 7, 34, m.facing)
+        --}
       end
       if m._label ~= nil then
-        m._label:position(m.x, m.y, m.z + OFFSET, m.polygon)
-        m._label.facing = m.facing
+        for i = 1, m._labellen do
+          m._label[i]:position(m.x - (i-1)*DIGIT_SPACE, m.y, m.z + OFFSET, m.polygon)
+          m._label[i].facing = m.facing
+        end
       end
     end
   end
