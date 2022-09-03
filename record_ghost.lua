27bc36390c20e771bd5b5e533ab05f663e9dc605
Zach Amador <zach.amador@gmail.com>
2022-09-02 19:01:53 -0700

    Record ghost changes

--- record_ghost.lua
+++ record_ghost.lua
@@ -1,30 +1,33 @@
+-- record_ghost.lua
+-- by aperturegrillz
+-- Records a "ghost" of a given play and outputs it as 'ghost.lua'
+
 Triggers = {}
 
 function Triggers.init()
 	Game.restore_passed()
-	if Players[0]._ghosts == nil then
-		Players[0]._ghosts = 0
+	if Players[0]._levels == nil then
+		Players[0]._levels = 0
 		
 		f = assert(io.open("ghost.lua", "w"))
 		f:write("-- Ghost exported by record_ghost.lua\n")
-		f:write("ghost = {\n")
+		f:write("ghost = {}\n")
+	else
+		Players[0]._levels = Players[0]._levels + 1
+		f = assert(io.open("ghost.lua", "a"))
 	end
+	f:write("ghost[" .. Level.index .. "]={\n")
 end
 
 function Triggers.idle()
-	if Players[0]._ghosts == 0 then
-		if Game.ticks > 0 then
-			f:write(",\n")
-		end
-		f:write("\t{x="..Players[0].x..
-				",y="..Players[0].y..
-				",z="..Players[0].z..
-				",p="..Players[0].polygon.index..
-				",f="..Players[0].direction.."}")
-	end
+	f:write("["..Game.ticks.."]={"..
+			"x="..Players[0].x..
+			",y="..Players[0].y..
+			",z="..Players[0].z..
+			",p="..Players[0].polygon.index..
+			",f="..Players[0].direction.."},\n")
 end
 
 function Triggers.cleanup()
-	Players[0]._ghosts = 1
-	f:write("\n}\n")
+	f:write("}\n")
 end
